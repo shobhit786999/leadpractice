7d85e8c790f49d7e2c1452016aad4433e50530cb
shobhit786999 <shobhitraghuwanshi07@gmail.com>
2026-08-06 22:46:12 +0530

    two addition

---
+++ add-two-no-by-addition.py
@@ -0,0 +1,14 @@
+# num1 = int(input("enter the first number "))
+# num2 = int(input("enter the second number "))
+# result = 0 
+# for value in range (num2):
+#     result+=num1
+    
+# print("mulyiplication",result)    
+# -------------------------------
+number = input("enter the number ")
+sum = 0 
+for i  in number:
+    sum = sum+int(i)
+    
+print("sum is : ",sum)    
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+num1 = int(input("enter the first number "))
+num2 = int(input("enter the second number "))
+result = 0 
+for value in range (num2):
+    result+=num1
+    
+print("mulyiplication",result) 
