8234e8c34678433994607231140fc40669982b07
shobhit786999 <shobhitraghuwanshi07@gmail.com>
2026-08-13 13:52:22 +0530

    class

---
+++ class.py
@@ -0,0 +1,12 @@
+class student:
+    def __init__(self,name,age,year):
+        self.name =name,
+        self.age =age,
+        self.year=year
+        
+        def generate(self):
+            print(name),
+            print(age),
+            print(year)
+s1=student("shobhit",54,2012)
+print(s1)
