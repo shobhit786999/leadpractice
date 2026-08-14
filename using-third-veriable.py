f7e863d239e247732aa9192bcf49e56598cb7c8b
shobhit786999 <shobhitraghuwanshi07@gmail.com>
2026-08-14 23:22:28 +0530

    second solution

--- using-third-veriable.py
+++ using-third-veriable.py
@@ -1,9 +1,16 @@
-x= int(input("enter the number is x  "))
-y = int (input("enter the number y "))
+# x= int(input("enter the number is x  "))
+# y = int (input("enter the number y "))
 
-temp = x
-print( "value of x ",temp)
-x = y 
-print(" new value of x ",x)
-y = temp
+# temp = x
+# print( "value of x ",temp)
+# x = y 
+# print(" new value of x ",x)
+# y = temp
+# print(y)
+x = int(input("enter the number x "))
+y = int(input("enter the number of y "))
+
+x,y=y,x
+
+print(x)
 print(y)
