b46f1791598cf6621d22a2da9249b53b85d08acb
shobhit786999 <shobhitraghuwanshi07@gmail.com>
2026-08-19 22:39:36 +0530

    compare three number

---
+++ threenumber.py
@@ -0,0 +1,13 @@
+
+num1 = int(input("enter the  num1"))
+num2 = int(input("enter the  num2"))
+num3 = int(input("enter the  num3"))
+
+if num1>num2 and num1>num3:
+    print(num1,"num1 is grater ")
+    
+elif num2>num1 and  num2>num3:
+    print(num2,"num2 is grater ")
+    
+else :
+    print(num3,"num 3 is grater ")    
