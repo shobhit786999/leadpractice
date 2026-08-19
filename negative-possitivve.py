5f5ecf3a48e1cde3762298d2fdefef8f4b979eb8
shobhit786999 <shobhitraghuwanshi07@gmail.com>
2026-08-19 22:35:47 +0530

    possitive and negative

---
+++ negative-possitivve.py
@@ -0,0 +1,10 @@
+
+num = int(input("enter the number "))
+if num > 0:
+    print("number is possitive ")
+    
+elif num<0:
+    print("number is negative ")
+    
+elif num==0:
+    print("number is zero ")    
