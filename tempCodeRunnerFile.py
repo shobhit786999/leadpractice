102ca8456760262223057249e1fb73d9aa67a863
shobhit786999 <shobhitraghuwanshi07@gmail.com>
2026-08-25 22:43:11 +0530

    temp code

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,11 @@
-month = int(input("enter the month"))
-calender = calendar.month(year,month)
-print(calendar)
+a = [[1,2,3],
+     [4,5,6]]
+t = [[0,0],
+     [0,0,],
+     [0,0]]
+for i in range(len(a)):
+    for j in range(len(a[0])):
+        t[j][i] = a[i][j]
+        
+for i in t:
+    print(i)    
