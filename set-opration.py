192e8f6fef431ea334ab280b3785456fa576c0df
shobhit786999 <shobhitraghuwanshi07@gmail.com>
2026-08-26 23:29:27 +0530

    perform set opration

---
+++ set-opration.py
@@ -0,0 +1,6 @@
+a = {1,2,3,4,5,6,7,8}
+b = {4,5,7,8,92,3,1}
+print("the union is ",a|b)
+print("the Intersection is ", a & b)
+print("the difference is ",a-b)
+print("the semmintic difference is ",a^b)
