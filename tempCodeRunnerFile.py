c4f7be2905f4bb4b616f9861cf1360769376c55f
shobhit786999 <shobhitraghuwanshi07@gmail.com>
2026-08-26 23:29:51 +0530

    demo

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,6 @@
-a = [[1,2,3],
-     [4,5,6]]
-t = [[0,0],
-     [0,0,],
-     [0,0]]
-for i in range(len(a)):
-    for j in range(len(a[0])):
-        t[j][i] = a[i][j]
-        
-for i in t:
-    print(i)    
+punc ='''!@#$%^&*();:{}[]-_~<>,\.?/'''
+string = input("enter anything here ")
+empty = ""
+for i in string:
+    if i not in punc:
+        empty+=i
