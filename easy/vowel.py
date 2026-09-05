f5355bfb9862f607c7c84f4533b0965f08343b4e
shobhit786999 <shobhitraghuwanshi07@gmail.com>
2026-09-05 22:55:45 +0530

    vowel

--- easy/vowel.py
+++ easy/vowel.py
@@ -1,3 +1,12 @@
+# # count vowel
+# text = input("enter the char : ")
+
+# count = 0
+# for char in text.lower():
+#     if char in "aeiou":
+#         count+=1
+        
+# print("number of vowel :",count)
 # count vowel
 text = input("enter the char : ")
 
